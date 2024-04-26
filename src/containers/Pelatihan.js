202286d887466007fd516f2c3ee98f50b09000d9
aldistamara <aldistie@gmail.com>
2024-04-26 14:13:21 +0700

    update 7 top page pelatihan

--- src/containers/Pelatihan.js
+++ src/containers/Pelatihan.js
@@ -8,24 +8,23 @@ const Pelatihan = () => {
     return (
         <>
             <Navbar />
-            <div className="self-stretch max-md:max-w-full pt-60 font-jost flex justify-center bg-white">
+            <div className="pl-40 pt-60 bg-white max-md:pl-5">
                 <div className="flex gap-5 max-md:flex-col max-md:gap-0">
                     <div className="flex flex-col w-[18%] max-md:ml-0 max-md:w-full">
                         <img
                             loading="lazy"
                             srcSet={Top}
                             alt=""
-                            className="grow shrink-0 max-w-full aspect-[0.79] w-[221px] max-md:mt-10"
+                            className="grow shrink-0 max-w-full aspect-[0.79] w-[271px] max-md:mt-10"
                         />
                     </div>
                     <div className="flex flex-col ml-5 w-[82%] max-md:ml-0 max-md:w-full">
                         <div className="flex flex-col self-stretch p-8 my-auto w-full text-white bg-sky-700 max-md:px-5 max-md:mt-10 max-md:max-w-full">
                             <div className="text-5xl font-bold leading-[57.6px] max-md:max-w-full max-md:text-4xl">
-                                Pelatihan
+                            Perizinan
                             </div>
                             <div className="mt-4 text-lg leading-7 max-md:max-w-full">
-                                Peserta akan dilatih berdasarkan kurikulum OK OCE dan akan
-                                digabungkan ke kelompok inkubasi melalui komunitas penggerak.
+                            Peserta akan dijelaskan proses perizinan sesuai kebutuhannya.
                             </div>
                         </div>
                     </div>
